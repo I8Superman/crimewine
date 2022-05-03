17284e43b3261392503fb8f3e3d2a892aa3d1727
Simon Andersen <simo414u@stud.kea.dk>
2022-05-03 15:53:47 +0200

    feat: basic layout of input boxes in place

--- src/components/Kurv/Forsendelse.js
+++ src/components/Kurv/Forsendelse.js
@@ -1,7 +1,67 @@
-
+import './Forsendelse.scss';
 
 export default function Forsendelse(props) {
     return (
-        <h2>Forsendelse</h2>
+        <div className="c-shipping">
+            <div className="c-shipping__breadcrumbs-and-order-btn">
+                <div className="c-shipping__breadcrumbs">
+                    <p className='c-shipping__breadcrumbs__indkøbskurv'>Indkøbskurv</p>
+                    <p className='c-shipping__breadcrumbs__information'>{'> Information og forsendelse'}</p>
+                    <p className='c-shipping__breadcrumbs__betal'>{'> Godkend og betal'}</p>
+                </div>
+            </div>
+            <form action="">
+                <div className="customer-info">
+                    <h3 className='c-shipping__header'>Adresseoplysninger</h3>
+                    <select className={'form-field full-width'} name="customertype" id="">
+                        <option value="private">Privat</option>
+                        <option value="company">Virksomhed</option>
+                        <option value="public">Offentlig virksomhed</option>
+                    </select>
+                    <input className={'form-field full-width'} type="email" Placeholder="E-mail" />
+                    <input className={'form-field half-width'} type="text" name="firstname" Placeholder="Fornavn" />
+                    <input className={'form-field half-width'} type="text" name="lastname" Placeholder="Efternavn" />
+                    <input className={'form-field full-width'} type="text" name="street-and-number" Placeholder="Adresse" />
+                    <input className={'form-field half-width'} type="text" name="postalcode" Placeholder="Postnummer" />
+                    <input className={'form-field half-width'} type="text" name="city" Placeholder="By" />
+                    <input className={'form-field half-width'} type="text" name="country" Placeholder="Land" />
+                    <input className={'form-field half-width'} type="tel" name="phonenumber" Placeholder="Telefonnummer" />
+                    <textarea className={'form-field full-width'} name="comments" Placeholder="Kommentarer til din ordre" />
+                </div>
+                <div className="shipping-options">
+                    <h3 className='c-shipping__header'>Forsendelse</h3>
+                    <div className='shipping-option'>
+                        <input type="radio" name="" id="" />
+                        <div classname="option-text">
+                            <h3 classname='option-title'>GLS PAkkeshop</h3>
+                            <p className='option-notes'>Nogle noter angående denne forsendelsesmetode</p>
+                        </div>
+                    </div>
+                    <div className='shipping-option'>
+                        <input type="radio" name="" id="" />
+                        <div classname="option-text">
+                            <h3 classname='option-title'>GLS PAkkeshop</h3>
+                            <p className='option-notes'>Nogle noter angående denne forsendelsesmetode</p>
+                        </div>
+                    </div>
+                    <div className='shipping-option'>
+                        <input type="radio" name="" id="" />
+                        <div classname="option-text">
+                            <h3 classname='option-title'>GLS PAkkeshop</h3>
+                            <p className='option-notes'>Nogle noter angående denne forsendelsesmetode</p>
+                        </div>
+                    </div>
+                    <div className='shipping-option'>
+                        <input type="radio" name="" id="" />
+                        <div classname="option-text">
+                            <h3 classname='option-title'>GLS PAkkeshop</h3>
+                            <p className='option-notes'>Nogle noter angående denne forsendelsesmetode</p>
+                        </div>
+                    </div>
+                </div>
+                {/* <input type="submit" name="Haarr" /> */}
+            </form>
+
+        </div>
     )
 }
--- src/components/Kurv/Oversigt.js
+++ src/components/Kurv/Oversigt.js
@@ -1,5 +1,6 @@
+import './Oversigt.scss';
+
 import { useContext, useEffect, useState } from 'react';
-import { Fragment } from "react/cjs/react.production.min";
 
 import dankort from '../../assets/images/payment-logos/logo-dankort.png';
 import mastercard from '../../assets/images/payment-logos/logo-mastercard.png';
@@ -31,50 +32,50 @@ export default function Oversigt(props) {
     console.log('Oversigt rendered');
 
     return (
-        <Fragment>
-            <div className="c-kurv__breadcrumbs-and-order-btn">
-                <div className="c-kurv__breadcrumbs">
-                    <p className='c-kurv__breadcrumbs__indkøbskurv'>Indkøbskurv</p>
-                    <p className='c-kurv__breadcrumbs__information'>{'> Information og forsendelse'}</p>
-                    <p className='c-kurv__breadcrumbs__betal'>{'> Godkend og betal'}</p>
+        <div className='c-oversigt'>
+            <div className="c-oversigt__breadcrumbs-and-order-btn">
+                <div className="c-oversigt__breadcrumbs">
+                    <p className='c-oversigt__breadcrumbs__indkøbskurv'>Indkøbskurv</p>
+                    <p className='c-oversigt__breadcrumbs__information'>{'> Information og forsendelse'}</p>
+                    <p className='c-oversigt__breadcrumbs__betal'>{'> Godkend og betal'}</p>
                 </div>
-                <button className='c-kurv__summary__ordering__go-to__top order-button'>Til bestilling</button>
+                <button className='c-oversigt__summary__ordering__go-to__top order-button'>Til bestilling</button>
             </div>
-            <div className='c-kurv__headers'>
-                <div className="c-kurv__header">Varer</div>
-                <div className="c-kurv__header">På lager</div>
-                <div className="c-kurv__header">Pris fl. / 6 fl.</div>
-                <div className="c-kurv__header">Antal</div>
-                <div className="c-kurv__header">Rabat</div>
-                <div className="c-kurv__header">I alt</div>
+            <div className='c-oversigt__headers'>
+                <div className="c-oversigt__header">Varer</div>
+                <div className="c-oversigt__header">På lager</div>
+                <div className="c-oversigt__header">Pris fl. / 6 fl.</div>
+                <div className="c-oversigt__header">Antal</div>
+                <div className="c-oversigt__header">Rabat</div>
+                <div className="c-oversigt__header">I alt</div>
             </div>
-            <div className="c-kurv__vare-container">
+            <div className="c-oversigt__vare-container">
                 {winesInBasket}
             </div>
-            <div className="c-kurv__summary">
-                <div className="c-kurv__summary__amount">
-                    <p className="c-kurv__summary__amount__text">Samlet beløb:</p>
-                    <p className="c-kurv__summary__amount__number">{totalPrice} DKK</p>
+            <div className="c-oversigt__summary">
+                <div className="c-oversigt__summary__amount">
+                    <p className="c-oversigt__summary__amount__text">Samlet beløb:</p>
+                    <p className="c-oversigt__summary__amount__number">{totalPrice} DKK</p>
                 </div>
-                <div className="c-kurv__summary__tax">
-                    <p className="c-kurv__summary__tax__text">Heraf moms:</p>
-                    <p className="c-kurv__summary__tax__number">{(totalPrice * 0.25).toFixed(2)} DKK</p>
+                <div className="c-oversigt__summary__tax">
+                    <p className="c-oversigt__summary__tax__text">Heraf moms:</p>
+                    <p className="c-oversigt__summary__tax__number">{(totalPrice * 0.25).toFixed(2)} DKK</p>
                 </div>
-                <div className="c-kurv__summary__total">
-                    <p className="c-kurv__summary__total__text">Total:</p>
-                    <p className="c-kurv__summary__total__number">{totalPrice} DKK</p>
+                <div className="c-oversigt__summary__total">
+                    <p className="c-oversigt__summary__total__text">Total:</p>
+                    <p className="c-oversigt__summary__total__number">{totalPrice} DKK</p>
                 </div>
-                <div className="c-kurv__summary__ordering">
-                    <div className='c-kurv__summary__ordering__accept'>
+                <div className="c-oversigt__summary__ordering">
+                    <div className='c-oversigt__summary__ordering__accept'>
                         <p>Vi tager imod:</p>
-                        <img className='c-kurv__summary__ordering__accept__payment-logo' src={dankort} alt="" />
-                        <img className='c-kurv__summary__ordering__accept__payment-logo' src={visa} alt="" />
-                        <img className='c-kurv__summary__ordering__accept__payment-logo' src={mastercard} alt="" />
-                        <img className='c-kurv__summary__ordering__accept__payment-logo' src={mobilePay} alt="" />
+                        <img className='c-oversigt__summary__ordering__accept__payment-logo' src={dankort} alt="" />
+                        <img className='c-oversigt__summary__ordering__accept__payment-logo' src={visa} alt="" />
+                        <img className='c-oversigt__summary__ordering__accept__payment-logo' src={mastercard} alt="" />
+                        <img className='c-oversigt__summary__ordering__accept__payment-logo' src={mobilePay} alt="" />
                     </div>
-                    <button className='c-kurv__summary__ordering__go-to order-button'>Til bestilling</button>
+                    <button className='c-oversigt__summary__ordering__go-to order-button'>Til bestilling</button>
                 </div>
             </div>
-        </Fragment>
+        </div>
     )
 }
