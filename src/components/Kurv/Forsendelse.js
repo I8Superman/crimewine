49321af20fb52573ecf05e649c1bc2f0d0636f62
Simon Andersen <simo414u@stud.kea.dk>
2022-06-01 14:21:33 +0200

    feat: Form validation optimized. Bottom submit button styled.

--- src/components/Kurv/Forsendelse.js
+++ src/components/Kurv/Forsendelse.js
@@ -4,7 +4,7 @@ import { useRef, useState } from 'react';
 
 export default function Forsendelse(props) {
 
-    const [currentFocus, setCurrentFocus] = useState('');
+    // const [currentFocus, setCurrentFocus] = useState('');
 
     const emailRef = useRef(null);
     const firstNameRef = useRef(null);
@@ -23,14 +23,14 @@ export default function Forsendelse(props) {
         notes: ''
     });
 
-    function applyFocus(e) { // Should this be done with refs (useRef) instead??
-        console.log(e.currentTarget.children[0])
-        const currentDiv = e.currentTarget.dataset.name;
-        setCurrentFocus(currentDiv);
-        // Targets the second child of the parent div - the actual input
-        // and focuses it, so the cursor will show up in the input field even if you click the parent
-        e.currentTarget.children[1].focus();
-    }
+    // function applyFocus(e) { // Should this be done with refs (useRef) instead??
+    //     console.log(e.currentTarget.children[0])
+    //     const currentDiv = e.currentTarget.dataset.name;
+    //     setCurrentFocus(currentDiv);
+    //     // Targets the second child of the parent div - the actual input
+    //     // and focuses it, so the cursor will show up in the input field even if you click the parent
+    //     e.currentTarget.children[1].focus();
+    // }
 
     function handleChange(e) {
         const validity = e.target.checkValidity();
@@ -49,7 +49,7 @@ export default function Forsendelse(props) {
         alert(JSON.stringify(shippingFormData));
     }
 
-    console.log('rendered')
+    console.log(shippingFormData)
 
     return (
         <div className="c-shipping">
@@ -59,13 +59,15 @@ export default function Forsendelse(props) {
                     <p className='c-shipping__breadcrumbs__information'>{'> Information og forsendelse'}</p>
                     <p className='c-shipping__breadcrumbs__betal'>{'> Godkend og betal'}</p>
                 </div>
+                <button type="submit" className='c-shipping__breadcrumbs__submit-button-top'>Til betaling</button>
             </div>
+
             <form onSubmit={handleSubmit}>
                 <fieldset className="customer-info">
                     <h3 className='customer-info__header'>Adresseoplysninger</h3>
                     <div className={'input-container select-container full-width'}>
                         <label htmlFor="customerType">Kundetype</label>
-                        <select className={'form-input select-input'} name="customertype" id="customerType" data-name='customertype' onClick={(e) => applyFocus(e)}>
+                        <select className={'form-input select-input'} name="customertype" id="customerType" >
                             <option value="private">Privat</option>
                             <option value="company">Virksomhed</option>
                             <option value="public">Offentlig virksomhed</option>
@@ -73,47 +75,47 @@ export default function Forsendelse(props) {
                     </div>
                     <div className={'input-container full-width'}>
                         <label htmlFor="email">E-mail*</label>
-                        <input ref={emailRef} className='form-input' type="email" id='email' name='email' value={shippingFormData.email} onChange={handleChange} placeholder="&nbsp;" pattern='(.+)\@(.+)\.(.+)' required />
+                        <input ref={emailRef} className='form-input' type="email" id='email' name='email' value={shippingFormData.email} onChange={handleChange} placeholder="&nbsp;" pattern='(.+)@(.+)\.(.){2,4}' required />
                         <span className="error" id="err-mail" aria-live="assertive">Skriv en korrekt email-adresse</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='firstname' onClick={(e) => applyFocus(e)} >
+                    <div className={'input-container half-width'}>
                         <label htmlFor="firstname">Fornavn*</label>
                         <input ref={firstNameRef} className='form-input' type="text" name="firstName" id='firstname' value={shippingFormData.firstName} onChange={handleChange} placeholder="&nbsp;" pattern='^[A-Z]\w*' required />
                         <span className="error" id="err-mail" aria-live="assertive">Husk: første bogstav med stort!</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='lastname' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container half-width'}>
                         <label htmlFor="lastname">Efternavn*</label>
                         <input ref={lastNameRef} className='form-input' type="text" name="lastName" id='lastname' value={shippingFormData.lastName} onChange={handleChange} placeholder="&nbsp;" pattern='^[A-Z]\w*' required />
                         <span className="error" id="err-lastname" aria-live="assertive">Husk: første bogstav med stort!</span>
                     </div>
-                    <div className={'input-container full-width'} data-name='address' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container full-width'}>
                         <label htmlFor="address">Adresse*</label>
                         <input className='form-input' type="text" name="address" id='address' placeholder="&nbsp;" pattern='^\D+(\d+)(.+)?' required />
                         <span className="error" id="err-adress" aria-live="assertive">Skriv en gyldig addresse</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='postalcode' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container half-width'}>
                         <label htmlFor="postalcode">Postnummer*</label>
                         <input className='form-input' type="text" name="postalcode" id='postalcode' placeholder="&nbsp;" pattern='\d{4}' required />
                         <span className="error" id="err-postal-code" aria-live="assertive">Indtast præcist 4 cifre</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='city' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container half-width'}>
                         <label htmlFor="city">By*</label>
                         <input className='form-input' type="text" name="city" id='city' placeholder="&nbsp;" pattern='^[A-Z]\w*' required />
                         <span className="error" id="err-city" aria-live="assertive">Bynavn med stort, tak.</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='country' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container half-width'}>
                         <label htmlFor="country">Land*</label>
                         <input className='form-input' type="text" name="country" id='country' placeholder="&nbsp;" pattern='^[A-Z]\w*' required />
                         <span className="error" id="err-country" aria-live="assertive">Skriv dit land (med stort)</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='phone' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container half-width'}>
                         <label htmlFor="phone">Telefonnummer</label>
                         <input className='form-input' type="tel" name="phone" id='phone' placeholder="&nbsp;" pattern='^(((00|0)|(\+))?(45)?)? ?(\d{2} ?){4}$' />
                         <span className="error" id="err-phone" aria-live="assertive">Skriv et korrekt telefonnummer</span>
                     </div>
-                    <div className={'input-container half-width'} data-name='notes' onClick={(e) => applyFocus(e)}>
+                    <div className={'input-container half-width'}>
                         <label htmlFor="notes">Noter</label>
-                        <textarea className={'form-input'} name="notes" id='notes' />
+                        <textarea className={'form-input'} name="notes" id='notes' placeholder="&nbsp;" />
                     </div>
                 </fieldset>
                 <fieldset className="shipping-options">
@@ -153,9 +155,8 @@ export default function Forsendelse(props) {
                         </div>
                         <p className='shipping-cost'>0,00 DKK</p>
                     </label>
+                    <button type="submit" className='shipping-options__submit-button-bottom'>Til betaling</button>
                 </fieldset>
-                {/* <input type="submit" name="Haarr" /> */}
-                <button type="submit">Submit</button>
             </form>
 
         </div >
